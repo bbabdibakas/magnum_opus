d585c1fcc3f784d6987fc926c03734ed1a545bcb
Bekzat Abdibakas <babdibakas@yandex.kz>
2024-07-29 23:30:16 +0500

    feat: installed react-router-dom, added pages Main, Profile

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,45 +1,21 @@
-import { useState } from "react"
 import "./App.css"
-import axios from "axios"
+import { Link, Route, Routes } from "react-router-dom"
+import MainPage from "../pages/MainPage"
+import ProfilePage from "../pages/ProfilePage"
 
 export const App = () => {
-    const [isAuth, setIsAuth] = useState(false)
-    const [username, setUsername] = useState('')
-    const [password, setPassword] = useState('')
-
-    const onLogin = async () => {
-        try {
-            const result = await axios.post('http://localhost:8000/auth/login', { username, password })
-
-            if (result.data) {
-                setIsAuth(true)
-            }
-        } catch (e) {
-            console.log(e)
-        }
-    }
-
     return (
         <div className="app">
             <div className="wrapper">
                 <div className="sidebar">
-                    sidebar
+                    <Link to="/">Main</Link>
+                    <Link to="/profile">Profile</Link>
                 </div>
                 <div className="content">
-                    <div className="auth">
-                        <div className="input">
-                            username:
-                            <input value={username} onChange={(e) => setUsername(e.target.value)} />
-                        </div>
-                        <div className="input">
-                            password:
-                            <input value={password} onChange={(e) => setPassword(e.target.value)} />
-                        </div>
-                        <button onClick={onLogin}>
-                            Login
-                        </button>
-                    </div>
-                    {isAuth && 'Logged!'}
+                    <Routes>
+                        <Route path="/" element={<MainPage />} />
+                        <Route path="/profile" element={<ProfilePage />} />
+                    </Routes>
                 </div>
             </div>
         </div>
