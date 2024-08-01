9a30a10c652b8e488f1028ad102bb7e581f7ce9a
Bekzat Abdibakas <babdibakas@yandex.kz>
2024-08-01 18:05:30 +0500

    fix: styling fix

--- client/src/shared/ui/AppLink/AppLink.tsx
+++ client/src/shared/ui/AppLink/AppLink.tsx
@@ -1,24 +1,37 @@
 import { Link, LinkProps } from 'react-router-dom'
 import cls from './AppLink.module.scss'
-import { ReactNode } from 'react'
+import { ReactNode, useState } from 'react'
 import { classNames } from 'shared/lib/classNames/classNames'
 
 interface AppLinkProps extends LinkProps {
     children: ReactNode
     className?: string
+    animationClassName?: string
 }
 
 export const AppLink = (props: AppLinkProps) => {
     const {
         children,
         className,
+        animationClassName,
         to,
         ...otherProps
     } = props
+
+    const [isClicked, setIsClicked] = useState(false);
+
+    const onClickHandler = () => {
+        if (animationClassName) {
+            setIsClicked(true);
+        }
+        setTimeout(() => setIsClicked(false), 1000);
+    };
+
     return (
         <Link
             to={to}
-            className={classNames(cls.AppLink, [className])}
+            onClick={onClickHandler}
+            className={classNames(cls.AppLink, [className], { [animationClassName]: isClicked })}
             {...otherProps}
         >
             {children}
